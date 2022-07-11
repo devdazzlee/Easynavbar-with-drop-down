6dc34907ae30c764b00068983665d0930e27644c
Ahmedraza <ahmedrazasaad6@gmail.com>
2022-07-11 13:19:46 -0700

    initail commitr

---
+++ app.js
@@ -0,0 +1,10 @@
+// alert("Ahmed Raza Jafri ");
+function raza() {
+
+
+ var a = document.getElementById("inp-1").value
+console.log(a)
+alert("Value has been submited")
+document.getElementById("inp-1").value = "";
+
+}
